cdd222f0e5b4ffb8e970925607e8aa83ad22de8d
caian23@outlook <caian23@outlook>
2025-02-12 14:26:41 -0300

    CHANGE: Mudando App para poder usar o gráfico

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import * as d3 from "d3";
+import { useState, useEffect } from "react";
+import LinePlot from './LinePlot';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const compras = [
+    'data/compras/Compra_2019_01.csv',
+    'data/compras/Compra_2019_02.csv',
+    'data/compras/Compra_2019_03.csv',
+    'data/compras/Compra_2019_04.csv',
+    'data/compras/Compra_2019_05.csv',
+    'data/compras/Compra_2019_06.csv'
+  ];
+  const meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho"];
+  
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function loadData() {
+      try {
+        const values = await Promise.all(compras.map(async (file) => {
+          const csvData = await d3.csv(file);
+          const sumIcms = csvData.reduce((acc, d) => acc + (+d.icms || 0), 0);
+          return sumIcms;
+        }));
+        setData(values);
+      } catch (error) {
+        console.error("Erro ao carregar os arquivos CSV:", error);
+      }
+    }
+
+    loadData();
+    
+  }, []);
+
+  
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Gráfico de ICMS por Mês</h1>
+      <LinePlot data={data} valoresx={meses} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
