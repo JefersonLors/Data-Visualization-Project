44ab9f4e9c29795597185ca517aa7b0c7f0ed3b0
caian23@outlook <caian23@outlook>
2025-02-15 11:34:19 -0300

    CHANGE: Mudança na lógica do loaddata

--- src/App.jsx
+++ src/App.jsx
@@ -37,28 +37,46 @@ function App() {
   const [vendasEstadosDestino, setVendasEstadosDestino] = useState([]);
 
   useEffect(() => {
+    async function sumData(csvData, column) {
+      const sumTotalBruto = csvData.reduce((acc, d) => acc + (+d[column] || 0), 0);
+      return sumTotalBruto;
+    }
+  
+    
     async function loadData(files, setData, column) {
       try {
-        const values = await Promise.all(files.map(async (file) => {
-          const csvData = await d3.csv(file);
-          const sumTotalBruto = csvData.reduce((acc, d) => acc + (+d[column] || 0), 0);
-          return sumTotalBruto;
-        }));
+        const values = await Promise.all(
+          files.map(async (file) => {
+            const csvData = await d3.csv(file);
+            switch (column) {
+              case "total_bruto":
+              case "icms":
+                return sumData(csvData, column);
+              default:
+                return 0;
+            }
+          })
+        );
         setData(values);
       } catch (error) {
         console.error("Erro ao carregar os arquivos CSV:", error);
       }
     }
-
+  
     loadData(comprasFiles, setComprasTotalBruto, "total_bruto");
     loadData(vendasFiles, setVendasTotalBruto, "total_bruto");
   }, []);
   
+  
   return (
     <>
-      <h2>Qual foi a evolução mensal do total bruto de compras e vendas?</h2>
-      <LinePlot compras={comprasTotalBruto} vendas={vendasTotalBruto} valoresx={meses} />
-    </>
+  <h2>Qual foi a evolução mensal do total bruto de compras e vendas?</h2>
+  <LinePlot compras={comprasTotalBruto} vendas={vendasTotalBruto} valoresx={meses} />
+  
+  {/* <h2>Qual a distribuição do ICMS entre os diferentes estados nas compras e vendas?</h2>
+  <BarPlot comprasEstadosOrigem={comprasEstadosOrigem} vendasEstadosDestino={vendasEstadosDestino} valoresx={meses} /> */}
+</>
+
   );
 }
 
